c33d9cb3a5af91424e581c769ebf73e0ce8ed980
wanderxrs <nyedsomwander@gmail.com>
2025-11-06 18:55:29 -0300

    carrossel e rodapé adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CarrosselComponent } from './carrossel/carrossel.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, CarrosselComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
