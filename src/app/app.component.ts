997886b9a4bf605f355d01b3afa869369ecf4c98
wanderxrs <nyedsomwander@gmail.com>
2025-11-08 14:08:57 -0300

    headers, rodapé, histótia, carrossel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { CarrosselComponent } from './carrossel/carrossel.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { HeaderAlternativoComponent } from './header-alternativo/header-alternativo.component';
+import { MossaHistoriaComponent } from './mossa-historia/mossa-historia.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent, CarrosselComponent, RodapeComponent, HeaderAlternativoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, CarrosselComponent, RodapeComponent, HeaderAlternativoComponent, MossaHistoriaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
