63132244cbe58feed09914a6ea6ecd49a05456c3
Guilherme <guilhermehenrike0412@gmail.com>
2025-11-10 21:04:48 -0300

    base da tela de registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TelaRegistroComponent } from './tela-registro/tela-registro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TelaRegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
