1ddae0d751caa884409345020c58350455d80dc1
Guilherme <guilhermehenrike0412@gmail.com>
2025-11-15 16:51:31 -0300

    ajuste em rotas

--- src/app/altcanc/altcanc.component.ts
+++ src/app/altcanc/altcanc.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HeaderAlternativoComponent } from '../header-alternativo/header-alternativo.component';
 
 @Component({
   selector: 'app-altcanc',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeaderAlternativoComponent],
   templateUrl: './altcanc.component.html',
   styleUrls: ['./altcanc.component.css']
 })
