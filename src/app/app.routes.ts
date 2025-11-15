c3bbedbf88ad069c6fa7f1a11964d7d4b50d5fed
wanderxrs <nyedsomwander@gmail.com>
2025-11-15 13:31:55 -0300

    rotas corrigidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,13 +12,10 @@ import { PoliticaDePrivacidadeComponent } from './politica-de-privacidade/politi
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ResultadoComponent } from './resultado/resultado.component';
-<<<<<<< HEAD
 import { ContatosComponent } from './contatos/contatos.component';
 import { PerguntasFrequentesComponent } from './perguntas-frequentes/perguntas-frequentes.component';
 import { InformacaoComponent } from './informacao/informacao.component';
-=======
-import { TelaRegistroComponent } from './tela-registro/tela-registro.component';
->>>>>>> 609cd19220d30598ab3fa5274830590bbd0f06ec
+
 
 export const routes: Routes = [
     { path: "opcoes-main", component: OpcoesMainComponent}, 
@@ -33,16 +30,11 @@ export const routes: Routes = [
     {path: "dashboard", component: DashboardComponent},
     {path: "home", component: HomeComponent},
     { path: 'resultado', component: ResultadoComponent },
-<<<<<<< HEAD
 
     {path: "contatos", component: ContatosComponent},
     {path: "pf", component: PerguntasFrequentesComponent},
     {path: "ic", component: InformacaoComponent}
    
-
-=======
-    { path: "registro", component: TelaRegistroComponent}
->>>>>>> 609cd19220d30598ab3fa5274830590bbd0f06ec
 
 
 
